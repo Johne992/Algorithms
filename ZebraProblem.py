e5c8116f22cfc48d59dd90096b4738f257e5eae8
John E <johnelewis92@gmail.com>
2017-07-07 19:47:57 -0500

    initial commit

---
+++ ZebraProblem.py
@@ -0,0 +1,10 @@
+#Logic to Solve the Zebra puzzle in Python
+
+#House lists should have 5 items. Color, Nationality, Drink, Pet, Cigarettes
+House1 = []
+House2 = []
+House3 = []
+House4 = []
+House5 = []
+#Street list should have 5 elements the houses
+Street = []
